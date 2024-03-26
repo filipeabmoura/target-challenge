c0f4115adf9af481875c363b703ca86db4dd13a6
filipeabmoura <filipemouradev@gmail.com>
2024-03-26 18:55:31 -0300

    tarefa 1 - teste Target Sistemas

---
+++ target-challenge-1/target-challenge-1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+1) Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+{
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+imprimir(SOMA);
+
+
+Ao final do processamento, qual será o valor da variável SOMA?
+
+======================> RESPOSTA:     91
+ * 
+ * */
+
+int INDICE = 13;
+int SOMA = 0;
+int K = 0;
+
+while (K < INDICE)
+{
+    K++;
+    SOMA += K;
+
+}
+
+Console.WriteLine(SOMA);
+
+// O valor da váriavel SOMA é 91
